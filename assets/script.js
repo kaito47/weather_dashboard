e4804324a39ff7589558582aebfd091748dde4c0
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-04-04 22:20:15 -0400

    updated css

--- assets/script.js
+++ assets/script.js
@@ -19,23 +19,16 @@
     // Here we grab the text from the input box
     var city = $("#citySearch").val().trim();
 
-
-    // // It then creates a new div for each drink. Note we create divs and add the content in the same line.
-    // var newDrinkDiv = $("<div>" + drinkList[i] + "</div>");
-
-    // // It then adds this new div to the drinkList div.
-    // drinkDiv.append(newDrinkDiv);
-
     
       // Here we construct our URL
       var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0d4895fd6417d7c738a2d7486562a71a";
 
       console.log(queryURL); 
-//  });
-//     // adding city to recently searched div -DOES NOT PREPEND
-    $("#recentlySearched").prepend("<div>" + city + "</div>");
+
+//     // adding city to recently searched div 
+    $("#recentlySearched").prepend("<div class='cities'>" + city + "</div>");
    
-// // <div id='recentSearches'>
+
 
 // // </div>
 // // var count = 0
@@ -47,12 +40,10 @@ $.ajax({
   url: queryURL,
   method: "GET",
 }).then(function(response) {
-  // $("#citySearch").text(JSON.stringify(response));
-  //   console.log(response);
+  $("#citySearch").text(JSON.stringify(response));
+    console.log(response);
 
-    // $("#recentlySearched").prepend(
-    //   `<div class="cityResult">
-    //   </div>`);
+ 
           
 // this kind of works but gets the whole object and doesn't prepend:
     // $.ajax({
