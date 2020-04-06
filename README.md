07d612936b9d93b9ad2bc43a0abb5f2a5b3f601d
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-04-05 23:45:51 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # weather_dashboard
+
+## Description:
+
+This weather site will allow the user to input any city of their choice into the search box, and to see the current day's weather as well as a five-day forecast for the chosen city. Weather data includes: temperature, humidity, wind speed (for current day only), and a weather icon for the forecasted days. 
+
+## Technologies:
+
+This planner was made using the following front-end technologies:
+-HTML
+-CSS
+-BOOTSTRAP
+-JQUERY
+-JAVASCRIPT
+-MOMENT.JS
+
+## Usage:
+
+To input a city, type the city of your choosing into the search box:
+
+![city search](https://github.com/kaito47/day_planner/blob/master/Assets/images/hover.png)r
+
+You will see your previously searched cities listed underneath the search bar:
+
+![previously searched](https://github.com/kaito47/day_planner/blob/master/Assets/images/save_button.png)
+
+## Features:
+
+The results page will show the currently searched city, with the temperature, humidity, wind speed, and UV index for that city. Below you will see the five-day forecast for that city, including the high temperature, teh weather icon for the day, and the humidity:
+
+![results](https://github.com/kaito47/day_planner/blob/master/Assets/images/colors.png)
+
+
+## Link:
+
+https://kaito47.github.io/weather_dashboard/
+
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,4 @@
-  // Function for displaying city
-  // function displayCity() {
 
-  
-    // $("#recentlySearched").empty();
-   
-    //      var cityName = $("<div>");
-         
-    //      // Adding the div to the HTML
-    //      $("#recentlySearched").append(cityName);
-         
-    //    }
-     
  $("#citySearch").val(" ");
  // This .on("click") function will trigger the AJAX Call
  $("#search").on("click", function(event) {
@@ -26,17 +14,12 @@
 
       console.log(queryURL); 
 
-//     // adding city to recently searched div 
-    $("#recentlySearched").prepend("<div class='cities'>" + city + "</div>");
-   
-
-
-// // </div>
-// // var count = 0
-// // This did NOT work to clear search bar:
-// //  var recentSearches = $("recentlySearched").val();
-// //  recentSearches = "";
     
+   // adding city to recently searched div 
+    $("#recentlySearched").prepend("<div class='cities'>" + city + "</div>");
+  
+
+
 $.ajax({
   url: queryURL,
   method: "GET",
@@ -48,10 +31,15 @@ $.ajax({
 
     console.log(response);
  
+ // adding date to city
+  var todayDate = (moment().format(' []M[/]D[/]YYYY[]'))
+
     // adding city name to results div:
-    // $("#results").append("<h1>" + response.name + "</h1>");
-    $("#cityName").html(response.name)
+ 
+    $("#cityName").html(response.name + ":" + todayDate);
     console.log(response.name);
+
+    
 
     // convert temp to fahrenheit and add to results div:
     var tempF = (response.main.temp -273.15) * 1.80 + 32;
@@ -62,7 +50,7 @@ $.ajax({
     $("#humidity").html("Humidity: " + response.main.humidity + " %");
 
     // adding wind speed to results div:
-    // $("#results").append("<h6>Wind Speed: " + response.wind.speed + " MPH" + "</h6>");
+
     $("#windSpeed").html("Wind Speed: " + response.wind.speed + " MPH");
     console.log(response.wind.speed);
 
@@ -86,14 +74,12 @@ $.ajax({
       console.log(response);
   });
 
-// }); 
+
 
 // constructing URL for forecast:
 var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
 
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={YOUR API KEY}
 
-// call to obtain forecast:
 
 $.ajax({
   url: queryURL,
@@ -109,51 +95,54 @@ $.ajax({
     console.log(response.daily[4]);
 
 
-    // loop to convert temp to fahrenheit and add to results div:
-    // let forecastTemps=[];
+      var tempFONE = (((response.daily[1].temp.day -273.15) * 1.80 + 32).toFixed(2));
+      var tempFTWO = (((response.daily[2].temp.day -273.15) * 1.80 + 32).toFixed(2));
+      var tempFTHREE = (((response.daily[3].temp.day -273.15) * 1.80 + 32).toFixed(2));
+      var tempFFOUR = (((response.daily[4].temp.day -273.15) * 1.80 + 32).toFixed(2));
+      var tempFFIVE = (((response.daily[5].temp.day -273.15) * 1.80 + 32).toFixed(2));
+   
 
-    // for (i=0; i < 5; i++) {
+    // to add dates:
+    var dateOne = (moment().add(1, 'days').format(' []M[/]D[/]YYYY[]'))
+    $("#dateONE").text(dateOne);
 
-      var tempFONE = (((response.daily[0].temp.day -273.15) * 1.80 + 32).toFixed(2));
-      var tempFTWO = (((response.daily[1].temp.day -273.15) * 1.80 + 32).toFixed(2));
-      var tempFTHREE = (((response.daily[2].temp.day -273.15) * 1.80 + 32).toFixed(2));
-      var tempFFOUR = (((response.daily[3].temp.day -273.15) * 1.80 + 32).toFixed(2));
-      var tempFFIVE = (((response.daily[4].temp.day -273.15) * 1.80 + 32).toFixed(2));
-    // console.log(tempF);
+    var dateTwo = moment().add(2, 'days').format(' []M[/]D[/]YYYY[]');
+    $("#dateTWO").text(dateTwo);
 
-    // forecastTemps.push(tempF);
-    
-    // console.log(forecastTemps);
+    var dateThree = moment().add(3, 'days').format(' []M[/]D[/]YYYY[]');
+    $("#dateTHREE").text(dateThree);
+
+    var dateFour = moment().add(4, 'days').format(' []M[/]D[/]YYYY[]');
+    $("#dateFOUR").text(dateFour);
+
+    var dateFive = moment().add(5, 'days').format(' []M[/]D[/]YYYY[]');
+    $("#dateFIVE").text(dateFive);
 
     // to add icons:
-    var iconCodeONE = response.daily[0].weather[0].icon;
-    var iconCodeTWO = response.daily[1].weather[0].icon;
-    var iconCodeTHREE = response.daily[2].weather[0].icon;
-    var iconCodeFOUR = response.daily[3].weather[0].icon;
-    var iconCodeFIVE = response.daily[4].weather[0].icon;
+    var iconCodeONE = response.daily[1].weather[0].icon;
+    var iconCodeTWO = response.daily[2].weather[0].icon;
+    var iconCodeTHREE = response.daily[3].weather[0].icon;
+    var iconCodeFOUR = response.daily[4].weather[0].icon;
+    var iconCodeFIVE = response.daily[5].weather[0].icon;
 
-    $('#dayOnepng').attr('src', "http://openweathermap.org/img/wn/" + iconCodeONE + ".png");
+
+    $('#dayOnepng').attr('src', "http://openweathermap.org/img/wn/" + iconCodeONE + ".png"); 
     $('#dayTwopng').attr('src', "http://openweathermap.org/img/wn/" + iconCodeTWO + ".png");
     $('#dayThreepng').attr('src', "http://openweathermap.org/img/wn/" + iconCodeTHREE + ".png");
     $('#dayFourpng').attr('src', "http://openweathermap.org/img/wn/" + iconCodeFOUR + ".png");
     $('#dayFivepng').attr('src', "http://openweathermap.org/img/wn/" + iconCodeFIVE + ".png");
 
-    // var iconurlONE = "http://openweathermap.org/img/wn/" + iconCodeONE + "@2x.png";
-    // var iconurlTWO = "http://openweathermap.org/img/wn/" + iconCodeTWO + "@2x.png";
-    // var iconurlTHREE = "http://openweathermap.org/img/wn/" + iconCodeTHREE + "@2x.png";
-    // var iconurlFOUR = "http://openweathermap.org/img/wn/" + iconCodeFOUR + "@2x.png";
-    // var iconurlFIVE = "http://openweathermap.org/img/wn/" + iconCodeFIVE + "@2x.png";
+  $(".weather").empty();
 
+    $("#weatherOne").append(`<li>${"Temp: " + tempFONE + " F"}</li> <li>${"Humidity: " + response.daily[1].humidity + "%"}</li>`)
 
-    $("#dayOne").append(`<li>${tempFONE + " F"}</li> <li>${response.daily[0].humidity + "%"}</li>`)
+    $("#weatherTwo").append(`<li>${"Temp: " + tempFTWO + " F"}</li> <li>${"Humidity: " + response.daily[2].humidity + "%"}</li>`);
 
-    $("#dayTwo").append(`<li>${tempFTWO + " F"}</li> <li>${response.daily[1].humidity + "%"}</li>`);
+    $("#weatherThree").append(`<li>${"Temp: " + tempFTHREE + " F"}</li> <li>${"Humidity: " + response.daily[3].humidity + "%"}</li>`);
 
-    $("#dayThree").append(`<li>${tempFTHREE + " F"}</li> <li>${response.daily[2].humidity + "%"}</li>`);
+    $("#weatherFour").append(`<li>${"Temp: " + tempFFOUR + " F"}</li> <li>${"Humidity: " + response.daily[4].humidity + "%"}</li>`);
 
-    $("#dayFour").append(`<li>${tempFFOUR + " F"}</li> <li>${response.daily[3].humidity + "%"}</li>`);
-
-    $("#dayFive").append(`<li>${tempFFIVE + " F"}</li> <li>${response.daily[4].humidity + "%"}</li>`);
+    $("#weatherFive").append(`<li>${"Temp: " + tempFFIVE + " F"}</li> <li>${"Humidity: " + response.daily[5].humidity + "%"}</li>`);
     }
 );
 
@@ -161,38 +150,5 @@ $.ajax({
 
 
 });
-// });
 
- // http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
-
-  // this kind of works but gets the whole object and doesn't prepend:
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET",
-    // }).then(function(response) {
-    //   $("#recentlySearched").text(JSON.stringify(response));
-    //     console.log(response);
-
-    //     $("#recentlySearched").prepend(
-    //       `<div class="cityResult">
-    //        <h3>${city}</h3>
-    //       </div>`);
-              
-
-//         $("#recentlySearched").prepend(
-//             `<div class="cityResult">
-//             <h3>${response.city}</h3>
-//             </div>`);
-    
-
-//         //end ari's code
-      // displayCity();
-  //  });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.modal');
-//   var instances = M.Modal.init(elems, options);
-// });
-
-// Or with jQuery
-
+ 
